337cfcaa23455bb7e36b5d2e32ff6a0753d56e06
simdeistud <simone.deiana@studenti.units.it>
2026-07-10 21:53:35 +0200

    Refactoring with wrappers

---
+++ src/Heuristic.py
@@ -0,0 +1,215 @@
+from utils.formulation import CustomerBasedFormulation, CVRPFormulation, SDVRPFormulation, HygeseFormulation
+import random
+import time
+import math
+import hygese as hgs
+
+def solve(dat_path: str):
+    instance = CustomerBasedFormulation()
+    instance.import_CmCTPRF(dat_path)
+
+    Vp = instance.Vp
+    W = instance.W
+    Ap = instance.Ap
+    c = instance.c
+    V_rank = instance.V_rank
+    V_sto = instance.V_sto
+    M = instance.M
+    d = instance.d
+    Q = instance.Q
+    sigma = instance.sigma
+    t_sto = instance.t_sto
+
+    def is_set_cover(V_sel: set[int]) -> bool:
+        cover_candidate: set[int] = set()
+        # For each node in V_sel, add the corresponding nodes in W whose demand it satisfies
+        for j in V_sel:
+            for i in W:
+                if j in V_rank[i]:
+                    cover_candidate.add(i)
+        return cover_candidate.issuperset(W)
+
+    def constructSet(Vp: set[int], W: set[int], V_rank: dict[int, list[int]], V_sto: set[int], sigma: int, Ap: set[tuple[int, int]], c: dict[tuple[int, int], float]) -> tuple[set[int], float]:
+        V_sel: set[int] = set()
+        W_cov: set[int] = set()
+
+        while not is_set_cover(V_sel):
+            i = random.choice(tuple(W.difference(W_cov)))  # Get a random uncovered node
+            j_star = -1
+            best_coverage = -1
+            # We search for the stop node j* that covers demand node i
+            # and which covers the most demand nodes than the others
+            for j in V_rank[i]:
+                W_j = {ip for ip in W if j in V_rank[ip]}  # Nodes in W that j can cover
+                coverage = len(W_cov.union(W_j))
+                if coverage > best_coverage:
+                    best_coverage = coverage
+                    j_star = j
+            # Add the best candidate to the solution and update covered nodes
+            V_sel.add(j_star)
+            W_j_star = {ip for ip in W if j_star in V_rank[ip]}
+            W_cov.update(W_j_star)  # Update covered nodes
+
+        # We remove redundant stop nodes if there are any
+        for j in V_sel.copy():
+            if is_set_cover(V_sel.difference({j})):
+                V_sel.remove(j)
+
+        # W_compl[j] is the subset of demand nodes whose favorite stop node in V_sel is j
+        W_compl: dict[int, set[int]] = {}
+        for j in V_sel:
+            W_compl[j] = set()
+        for i in W:
+            for j in V_rank[i]:
+                # Since V_rank is an ordered list, we just need to find 
+                # the first stop node in V_sel to find the best
+                if j in V_sel:
+                    W_compl[j].add(i)
+                    break
+        # V_alt[j] is the set of alternative stop nodes != j which cover W_compl[j]
+        V_alt: dict[int, set[int]] = {}
+        for j in V_sel:
+            V_alt[j] = set()
+            for jp in V_sto.difference({j}):
+                W_j_p = {i for i in W if jp in V_rank[i]}
+                if W_compl[j].issubset(W_j_p):
+                    V_alt[j].add(jp)
+
+        g: dict[int, frozenset[int]] = {j : frozenset({j}.union(V_alt[j])) for j in V_sel}
+        G_avail: set[frozenset[int]] = set()
+        for j in V_sel:
+            G_avail.add(g[j])
+
+        j = random.choice(tuple(V_sel))
+        giantTour = [sigma, j, sigma]
+        giantTour_c = c[(sigma, j)] + c[(j, sigma)]
+        for group in G_avail.copy():
+            if j in group:
+                G_avail.remove(group)
+        while len(G_avail) > 0:
+            position = random.choice(["before", "after"])
+            
+            if position == "before":
+                j = giantTour[1]
+                s: dict[tuple[int, int], float] = {}
+                l: dict[frozenset[int], float] = {}
+                for group in G_avail:
+                    shortest: float = math.inf
+                    for z in group:
+                        current: float = c[(sigma, z)] + c[(z, sigma)]
+                        if current < shortest:
+                            shortest = current
+                            l[group] = shortest
+                    for jp in group:
+                        s[(jp, j)] = - l[group] - c[(sigma, j)] + c[(sigma, jp)] + c[(jp, j)]
+                j_star = -1
+                cost = math.inf
+                for arc in s:
+                    if s[arc] < cost:
+                        cost = s[arc]
+                        j_star = arc[0]
+                giantTour.insert(giantTour.index(j), j_star)
+                giantTour_c += s[j_star, j]
+                for group in G_avail.copy():
+                    if j_star in group:
+                        G_avail.remove(group)
+
+            if position == "after":
+                j = giantTour[-2]
+                s: dict[tuple[int, int], float] = {}
+                l: dict[frozenset[int], float] = {}
+                for group in G_avail:
+                    shortest: float = math.inf
+                    for z in group:
+                        current: float = c[(sigma, z)] + c[(z, sigma)]
+                        if current < shortest:
+                            shortest = current
+                            l[group] = shortest
+                    for jp in group:
+                        s[(j, jp)] = - l[group] - c[(j, sigma)] + c[(j, jp)] + c[(jp, sigma)]
+                j_star = -1
+                cost = math.inf
+                for arc in s:
+                    if s[arc] < cost:
+                        cost = s[arc]
+                        j_star = arc[1]
+                giantTour.insert(giantTour.index(j) + 1, j_star)
+                giantTour_c += s[j, j_star]
+                for group in G_avail.copy():
+                    if j_star in group:
+                        G_avail.remove(group)
+        cost_V_sel = giantTour_c
+        return V_sel, cost_V_sel
+
+    best_set_covers: dict[frozenset[int], float] = {}
+    treated_set_covers: dict[frozenset[int], float] = {}
+    consecutive_without_improvement = 0
+    elapsed_time = 0
+    TIME_LIMIT: int = 60 * 60 * 3 # 3H LIMIT
+    bestSol = (None, math.inf)
+    while elapsed_time < TIME_LIMIT and consecutive_without_improvement < 100:
+        iteration_start_time = time.time()
+        # PHASE 1
+        s, V_sel_cost = constructSet(Vp, W, V_rank, V_sto, sigma, Ap, c)
+        V_sel = frozenset(s)
+        if V_sel in treated_set_covers:
+            if random.choice([0, 1]) == 0: continue # We penalize by skipping resolution with a random chance
+        else:
+            for cover in best_set_covers:
+                if V_sel_cost < best_set_covers[cover]:
+                    best_set_covers[V_sel] = V_sel_cost
+                    break
+        if len(best_set_covers) == 0:
+            best_set_covers[V_sel] = V_sel_cost
+        # PHASE 2
+        V_sel: frozenset[int] = frozenset()
+        V_sel_cost = math.inf
+        for cover in best_set_covers:
+            if best_set_covers[cover] < V_sel_cost:
+                V_sel = cover
+                V_sel_cost = best_set_covers[cover]
+        V_sel_cost = best_set_covers.pop(V_sel)
+        treated_set_covers[V_sel] = V_sel_cost
+
+        # TransformSDVRPassociatedwith𝑉sel intoaCVRP
+        SDVR = SDVRPFormulation()
+        SDVR.import_CBF(instance, set(V_sel))
+        CVRP = CVRPFormulation()
+        CVRP.import_SDVRPF(SDVR)
+
+        # Findsolutionof theCVRPwithHGS-CVRP:CVRPSol
+        data = dict()
+        HGS = HygeseFormulation()
+        HGS.import_CVRPF(CVRP)
+        data['distance_matrix'] = HGS.distance_matrix
+        data['num_vehicles']  = HGS.num_vehicles
+        data['depot'] = HGS.depot
+        data['demands'] = HGS.demands
+        data['vehicle_capacity'] = HGS.vehicle_capacity
+        data['service_times'] = HGS.service_times
+        ap = hgs.AlgorithmParameters(nbIter=10000) # N. of iterations without improvement
+        hgs_solver = hgs.Solver(parameters=ap, verbose=True)
+        CVRPSol = hgs_solver.solve_cvrp(data)
+
+        if CVRPSol.cost < bestSol[1]:
+            bestSol = (CVRPSol, CVRPSol.cost)
+            consecutive_without_improvement = 0
+        # TransformCVRPSol intoaSDVRPsolution:SDVRPSol
+        #SDVRPSol: set[int] = set()
+        #SDVRPSol_cost: float = 0
+        #if SDVRPSol_cost < bestSol[1]:
+        # bestSol = (SDVRPSol, SDVRPSol_cost)
+        consecutive_without_improvement += 1
+        iteration_end_time = time.time()
+        elapsed_time += iteration_end_time - iteration_start_time
+
+    return bestSol[0].cost, elapsed_time
+
+            
+
+        
+                
+            
+
+
+
---
+++ src/RNF.py
@@ -0,0 +1,191 @@
+import gurobipy as gp
+from gurobipy import GRB
+from utils.formulation import RoadNetworkFormulation
+
+def solve(dat_path: str):
+    # Create model
+    model = gp.Model("road-network_formulation")
+    VALID_INEQUALITY_ENABLED = True
+
+    instance = RoadNetworkFormulation()
+    instance.import_CmCTPRF(dat_path)
+
+    M = instance.M
+    d = instance.d
+    Q = instance.Q
+    sigma = instance.sigma
+    t_sto = instance.t_sto
+    V = instance.V
+    W = instance.W
+    A = instance.A
+    c = instance.c
+    V_rank = instance.V_rank
+    V_sto = instance.V_sto
+
+
+    # 1l
+    z = model.addVars([(i, j) for i in W for j in V_rank[i]], vtype=GRB.BINARY, name="z")
+    # 1k
+    y = model.addVars([(j, k) for j in V_sto for k in M], vtype=GRB.BINARY, name="y")
+    # 1j
+    x = model.addVars([(arc[0], arc[1], k) for arc in A for k in M], vtype=GRB.INTEGER, lb=0, name="x")
+
+    q = model.addVars([(j, k) for j in V_sto for k in M], vtype=GRB.CONTINUOUS, lb=0, name="q")
+    f = model.addVars([(arc[0], arc[1], k) for arc in A for k in M], vtype=GRB.CONTINUOUS, lb=0, name="f")
+
+
+    # 1b
+    model.addConstrs(
+        (
+            gp.quicksum(z[i, j] for j in V_rank[i]) == 1
+            for i in W
+        ),
+        name="1b"
+    )
+    # 1c
+    model.addConstrs(
+        (
+            gp.quicksum(z[i, jp] for jp in V_rank[i] if instance.rank(i, jp) > instance.rank(i, j))
+            <= 1 - y[j, k]
+            for i in W
+            for j in V_rank[i]
+            for k in M
+        ),
+        name="1c"
+    )
+    # 1d
+    model.addConstrs(
+        (
+            gp.quicksum(d[i]*z[i, j] for i in W if j in V_rank[i])
+            == gp.quicksum(q[j, k] for k in M)
+            for j in V_sto
+        ),
+        name="1d"
+    )
+    # 1e
+    model.addConstrs(
+        (
+            gp.quicksum(q[j, k] for j in V_sto)
+            <= Q
+            for k in M
+        ),
+        name="1e"
+    )
+    # 1f
+    model.addConstrs(
+        (
+            gp.quicksum(x[h, j, k] for h in V if (h, j) in A) 
+            >= y[j, k]
+            for j in V_sto
+            for k in M
+        ),
+        name="1f"
+    )
+    # 1g
+    model.addConstrs(
+        (
+            gp.quicksum(x[hp, h, k] for hp in V if (hp, h) in A) 
+            - gp.quicksum(x[h, hp, k] for hp in V if (h, hp) in A) 
+            == 0
+            for h in V
+            for k in M
+        ),
+        name="1g"
+    )
+    # 1h
+    model.addConstrs(
+        (
+            gp.quicksum(f[h, hp, k] for hp in V if (h, hp) in A)
+            - gp.quicksum(f[hp, h, k] for hp in V if (hp, h) in A)
+            == q[h, k]
+            for k in M
+            for h in V_sto
+        ),
+        name="1h_sto"
+    )
+    model.addConstrs(
+        (
+            gp.quicksum(f[h, hp, k] for hp in V if (h, hp) in A)
+            - gp.quicksum(f[hp, h, k] for hp in V if (hp, h) in A)
+            == 0
+            for k in M
+            for h in V.difference(V_sto.union({sigma}))
+        ),
+        name="1h_nonsto"
+    )
+
+    # 1i
+    model.addConstrs(
+        (
+            gp.quicksum(f[h, sigma, k] for h in V if (h, sigma) in A) 
+            == gp.quicksum(q[j, k] for j in V_sto)
+            for k in M
+        ),
+        name="1i"
+    )
+
+    # 1m
+    model.addConstrs(
+        (
+            q[j, k] <= Q*y[j, k]
+            for j in V_sto
+            for k in M
+        ),
+        name="1m"
+    )
+    # 1n
+    model.addConstrs(
+        (
+            f[arc[0], arc[1], k] <= Q*x[arc[0], arc[1], k]
+            for arc in A
+            for k in M
+        ),
+        name="1n"
+    )
+
+    # VALID INEQUALITIES
+    if VALID_INEQUALITY_ENABLED:
+        s = model.addVars(V_sto, vtype=GRB.BINARY, name="s")
+
+        # 2a
+        model.addConstrs(
+            (
+                s[j] <= gp.quicksum(y[j, k] for k in M)
+                for j in V_sto
+            ),
+            name="2a"
+        )
+
+        # 2b
+        model.addConstrs(
+            (
+                s[j] >= y[j, k]
+                for j in V_sto
+                for k in M
+            ),
+            name="2b"
+        )
+
+        # 2c
+        model.addConstr(
+            (
+                gp.quicksum(y[j, k] - s[j] for j in V_sto for k in M) <= len(M) - 1
+            ),
+            name="2c"
+        )
+
+    # 1a
+    model.setObjective(
+        gp.quicksum(c[arc]*x[arc[0], arc[1], k] for k in M for arc in A) 
+        + gp.quicksum(t_sto*y[j, k] for k in M for j in V_sto), 
+        GRB.MINIMIZE
+    )
+
+    # Optimize
+    TimeLimit = 60 * 60 * 3 # 3H LIMIT
+    model.setParam("TimeLimit", TimeLimit)
+    model.optimize()
+
+    return model.objVal, model.Runtime
+
+
